10fa638c686e59a059642511fb561fd74cb09d50
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-22 02:19:08 -0300

    fix deps

--- .prettierrc.js
+++ .prettierrc.js
@@ -39,45 +39,4 @@ module.exports = {
       },
     },
   ],
-};module.exports = {
-  // Basic formatting options
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  trailingComma: 'all',
-  tabWidth: 2,
-  useTabs: false,
-  printWidth: 100,
-  
-  // JSX specific options
-  jsxSingleQuote: true,
-  jsxBracketSameLine: false,
-  
-  // Other options
-  arrowParens: 'avoid',
-  bracketSpacing: true,
-  endOfLine: 'lf',
-  
-  // File-specific overrides
-  overrides: [
-    {
-      files: '*.json',
-      options: {
-        singleQuote: false,
-      },
-    },
-    {
-      files: '*.scss',
-      options: {
-        singleQuote: false,
-      },
-    },
-    {
-      files: '*.md',
-      options: {
-        printWidth: 80,
-        proseWrap: 'always',
-      },
-    },
-  ],
 };
